c9669649d723541828a6179125468ac64aec7d2d
petarpejic97 <petar.pejic@outlook.com>
2020-05-13 12:59:33 +0200

    napravljen livedata

--- PrviZadatak/app/src/main/java/com/example/ptice/BirdCounterViewModel.kt
+++ PrviZadatak/app/src/main/java/com/example/ptice/BirdCounterViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.ptice
 
 import android.graphics.Color
+import androidx.databinding.InverseMethod
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class BirdCounterViewModel : ViewModel() {
-     var counter: String = 0.toString()
-     var background = R.color.colorTransparent
+    var counter =  MutableLiveData<Int>()
+    var background = R.color.colorTransparent
 
 
 }
--- PrviZadatak/app/src/main/java/com/example/ptice/MainActivity.kt
+++ PrviZadatak/app/src/main/java/com/example/ptice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ptice
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.ptice.databinding.ActivityMainBinding
@@ -17,28 +18,39 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel : BirdCounterViewModel
 
-    private lateinit var viewModelFactory: CounterViewModelFactory
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        viewModel = ViewModelProviders.of(this).get(BirdCounterViewModel::class.java)
+
         setUpUI()
+
+
+
+        viewModel.counter.observe(this,androidx.lifecycle.Observer {
+            binding.invalidateAll()
+        })
 
     }
 
     private fun setUpUI(){
-        binding.blueColor.setOnClickListener { saveColorAndIncrement(R.color.colorBlue) }
-        binding.greenColor.setOnClickListener { saveColorAndIncrement(R.color.colorPrimary) }
-        binding.greyColor.setOnClickListener { saveColorAndIncrement(R.color.colorGray) }
-        binding.brownColor.setOnClickListener { saveColorAndIncrement(R.color.colorBrown) }
-        binding.reset.setOnClickListener { resetCounterAndColor(R.color.colorTransparent) }
-
-        viewModel = ViewModelProviders.of(this).get(BirdCounterViewModel::class.java)
-
-        binding.bird = viewModel
+        if(viewModel.counter.value == null){
+            viewModel.counter.value=0
+        }
+        binding.apply {
+            bird = viewModel
+            binding.blueColor.setOnClickListener { saveColorAndIncrement(R.color.colorBlue) }
+            binding.greenColor.setOnClickListener { saveColorAndIncrement(R.color.colorPrimary) }
+            binding.greyColor.setOnClickListener { saveColorAndIncrement(R.color.colorGray) }
+            binding.brownColor.setOnClickListener { saveColorAndIncrement(R.color.colorBrown) }
+            binding.reset.setOnClickListener { resetCounterAndColor(R.color.colorTransparent) }
+        }
     }
+
     private fun saveColorAndIncrement(color : Int){
         setColor(color)
         incrementNumber()
@@ -47,20 +59,15 @@ class MainActivity : AppCompatActivity() {
     private fun setColor(color:Int){
         binding.bird?.background = color
     }
-   private fun incrementNumber() {
-        var counter = viewModel.counter.toInt()
-        counter++
-        viewModel.counter = counter.toString()
-        refreshUI()
-    }
-    private fun refreshUI() {
-        binding.invalidateAll()
+    private fun incrementNumber() {
+        var number = viewModel.counter.value
+        if (number != null) {
+            viewModel.counter.value = number+1
+        }
     }
     private fun resetCounterAndColor(color : Int){
         setColor(color)
-        binding.bird?.counter = "0"
-        refreshUI()
-        birdCounter.setBackgroundColor(Color.TRANSPARENT)
+        binding.bird?.counter?.value = 0
     }
 
 }
